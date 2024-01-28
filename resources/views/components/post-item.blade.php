7cb0f5536fc7ed0a6b0a5ac54661e0f5fec17f94
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2024-01-28 16:55:27 +0500

    add post-item component

---
+++ resources/views/components/post-item.blade.php
@@ -0,0 +1,32 @@
+<article class="bg-white flex flex-col shadow my-4">
+    <!-- Article Image -->
+    <a href="{{ route('view', $post) }}" class="hover:opacity-75">
+        <img src="{{ $post->getThumbnail()}}">
+        {{--        <img src="/storage/{{ $post->thubnail }}">--}}
+    </a>
+    <div class="bg-white flex flex-col justify-start p-6">
+        <div class="flex gap-4">
+            @foreach($post->categories as $category)
+                <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">
+                    {{ $category->title }}
+                </a>
+            @endforeach
+        </div>
+        <a href="{{ route('view', $post) }}" class="text-3xl font-bold hover:text-gray-700 pb-4">
+            {{ $post->title }}
+        </a>
+        <p href="#" class="text-sm pb-3">
+            Автор: <a href="#" class="font-semibold hover:text-gray-800">
+                {{ $post->user->name }}
+            </a>
+            <br/>
+            Опубликовано в {{ $post->getFormattedDate() }}
+        </p>
+        <a href="{{ route('view', $post) }}" class="pb-6">
+            {{ $post->shortBody() }}
+        </a>
+        <a href="#" class=" text-gray-800 hover:text-black">
+            Подробнее... <i class="fas fa-arrow-right"></i>
+        </a>
+    </div>
+</article>
