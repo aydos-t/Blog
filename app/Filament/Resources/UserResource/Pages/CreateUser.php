e99c9b867aee982e3dfd8f447b11bf2835316d68
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2024-02-02 15:17:01 +0500

    add users verification

---
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use App\Models\User;
+use Carbon\Carbon;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+
+class CreateUser extends CreateRecord
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['email_verified_at'] = Carbon::now();
+        $data['password'] = bcrypt($data['password']);
+        return $data;
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        /** @var User $user */
+        $user = parent::handleRecordCreation($data);
+        $user->assignRole('admin');
+
+        return $user;
+    }
+}
